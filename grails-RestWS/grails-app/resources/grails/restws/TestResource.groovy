869233dedca513093af894c630b033b5b7048a04
Raghu Gorur <raghu@onehaystack.com>
2014-01-18 03:44:11 -0800

    JSR 311 RESTful WS - jaxrs 0.8 plugin

---
+++ grails-RestWS/grails-app/resources/grails/restws/TestResource.groovy
@@ -0,0 +1,15 @@
+package grails.restws
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+
+@Path('/api/test')
+class TestResource {
+
+    @GET
+    @Produces('text/plain')
+    String getTestRepresentation() {
+        'Test'
+    }
+}
