37a94fbe3ec68635d1dc136e9096eb22ddb8ee4f
Raghu Gorur <raghu@onehaystack.com>
2014-01-22 17:09:10 -0800

    Added scaffold rest services

--- grails-RestWS/grails-app/resources/grails/restws/TestResource.groovy
+++ grails-RestWS/grails-app/resources/grails/restws/TestResource.groovy
@@ -3,13 +3,14 @@ package grails.restws
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam;
 
 @Path('/api/test')
 class TestResource {
 
     @GET
     @Produces('text/plain')
-    String getTestRepresentation() {
-        'Test'
+    String getTestRepresentation(@QueryParam('name') String name) {
+        "Hello ${name ? name : 'unknown'}"
     }
 }
